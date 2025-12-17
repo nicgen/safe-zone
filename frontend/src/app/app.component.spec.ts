8ceb32f2663fd2b8a9d9404e017a9cde641320dd
nicgen <nic@genty.dev>
2025-12-17 09:06:01 +0100

    TEST

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     // expect(app).toBeTruthy();
-    expect(app).toBeFalsy();
+    expect(app).toBeTruthy();
 
   });
 
