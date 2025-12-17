5d8df576634c0378b7bc571f6d4e986682011975
nicgen <nic@genty.dev>
2025-12-17 09:28:33 +0100

    TEST

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -13,8 +13,8 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // expect(app).toBeTruthy();
-    expect(app).toBeFalsy();
+    expect(app).toBeTruthy();
+    // expect(app).toBeFalsy(); // This will fail
 
   });
 
